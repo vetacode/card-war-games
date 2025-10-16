ba4101ef712b51e0f4b95d924147da5675e9120c
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-10-16 22:18:29 +0700

    add voters

--- try.js
+++ try.js
@@ -117,9 +117,19 @@ console.log(peopleWithPets2);
  * 1. Select the button in the DOM and add an event listener to it without saving the DOM element as a separate variable. I.e. "chain" the `addEventListener` on after your `getElementById()`(When clicked, log "Clicked" to the console)
  *    - I realize this might feel like busywork, but my intent will make sense soon
  * 
- * 2. Upcoming...
+ *  2. Given the array below, chain the `.filter` and `.map` array methods together to turn the array into an array of string email addresses of only the people in the array who voted. Log the array of email addresses to the console
  */
 
 
 
-document.getElementById('deck').addEventListener('click', handleClick);
+// document.getElementById('deck').addEventListener('click', handleClick);
+
+const voters = [
+  {name: "Joe", email: "joe@joe.com", voted: true},
+  {name: "Jane", email: "jane@jane.com", voted: true},
+  {name: "Bo", email: "bo@bo.com", voted: false},
+  {name: "Bane", email: "bane@bane.com", voted: false}
+]
+
+const personVoted = voters.filter(p => p.voted).map(e => e.email).forEach(d => console.log(d))
+// console.log(personVoted) 
