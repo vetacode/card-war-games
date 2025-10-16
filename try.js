56e04063bde02615ce0147627d27e3d6757c54bc
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-10-16 22:06:59 +0700

    add button async

--- try.js
+++ try.js
@@ -109,3 +109,17 @@ function filterArray(array, callback) {
 const peopleWithPets2 = filterArray(people2, (person) => person.hasPet);
 
 console.log(peopleWithPets2);
+
+
+/**
+ * Challenge: method chaining!
+ * 
+ * 1. Select the button in the DOM and add an event listener to it without saving the DOM element as a separate variable. I.e. "chain" the `addEventListener` on after your `getElementById()`(When clicked, log "Clicked" to the console)
+ *    - I realize this might feel like busywork, but my intent will make sense soon
+ * 
+ * 2. Upcoming...
+ */
+
+
+
+document.getElementById('deck').addEventListener('click', handleClick);
