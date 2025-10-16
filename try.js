dbfa39bf03cfa569c8c7e418cd42b44305d902a3
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-10-16 12:05:27 +0700

    add filter

--- try.js
+++ try.js
@@ -25,3 +25,22 @@ function callback() {
 }
 
 setTimeout(callback, 3000);
+
+/**
+ * Challenge:
+ *
+ * Part 1: Given the array of objects below, create a new array with the `.filter()` array method that contains only the objects where "hasPet" is true
+ *
+ *
+ * Part 2: Move the anonymous in-line function to its own, named function
+ */
+
+const people = [
+  { name: 'Jack', hasPet: true },
+  { name: 'Jill', hasPet: false },
+  { name: 'Alice', hasPet: true },
+  { name: 'Bob', hasPet: false },
+];
+
+const peopleWithPets = people.filter((person) => person.hasPet);
+console.log(peopleWithPets);
