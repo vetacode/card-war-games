8390a8df2c83c6d66b8fd66b899790288d42550c
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-10-17 09:15:31 +0700

    draw new deck

--- try.js
+++ try.js
@@ -178,8 +178,28 @@ fetch("https://apis.scrimba.com/bored/api/activity")
   
   document.getElementById("new-deck").addEventListener("click", handleClick)
 
+/**
+ * Challenge
+ * 
+ * Task: Using the saved deckId, draw 2 new cards from the deck
+ * 
+ * Docs for original Deck of Cards API: https://deckofcardsapi.com/#draw-card
+ * BaseUrl you'll use: https://apis.scrimba.com/deckofcards/api/deck/
+ * (that will replace the base url of https://deckofcardsapi.com/api/deck/)
+ * that you'll see in the deck of cards API docs.
+ */
 
+function handleClick() {
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/{deck_id}/draw/?count=2")
+      .then(res => res.json())
+      .then(data => 
+        {
+          const deckId = data.deck_id
+          console.log(deckId)
+        })
+}
 
+document.getElementById("draw-deck").addEventListener("click", handleClick)
 
 
 
