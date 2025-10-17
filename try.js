d309504f4853b93de40f725028555ce9cad456be
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-10-17 14:16:35 +0700

    adding theWInnerIs

--- try.js
+++ try.js
@@ -219,3 +219,46 @@ async function handleDraw() {
 }
 
 drawCardBtn.addEventListener('click', handleDraw);
+
+/**
+ * Challenge:
+ *
+ * Try to determine which of the 2 cards is the "winner" (has higher value)
+ * Aces are the card with the highest "score"
+ *
+ * In parts:
+ *
+ * 1. Create a function that takes 2 card objects as parameters,
+ * `card1` and `card2`. These card objects have a property called
+ * `value`, which can be any one of the following strings, in
+ * order of rising "score":
+ *
+ * "2", "3", "4", "5", "6", "7", "8", "9",
+ * "10", "JACK", "QUEEN", "KING", "ACE"
+ *
+ * I.e. "2" is the lowest score and "ACE" is the highest.
+ *
+ * The function should determine which of the 2 cards (`card1`
+ * or `card2`) has the higher score, or if they have the same score.
+ *
+ * Log which card wins (or "It's a tie!"
+ * if they're the same) to the console
+ */
+
+function theWinnerIs(card1, card2) {
+  const valueOptions = [
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'JACK',
+    'QUEEN',
+    'KING',
+    'ACE',
+  ];
+}
